9eacc30ae212a50a69c00ce84d4d998199aafa1b
Abdullah Al Mamun <bdmamun100@gmail.com>
2025-02-22 16:24:20 +0600

    setup layout, routes and redux

---
+++ src/pages/admin/CreateAdmin.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CreateAdmin = () => {
+    return (
+        <div>
+            Admin
+        </div>
+    );
+};
+
+export default CreateAdmin;
---
+++ src/pages/admin/CreateFaculty.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CreateFaculty = () => {
+    return (
+        <div>
+            Faculty
+        </div>
+    );
+};
+
+export default CreateFaculty;
